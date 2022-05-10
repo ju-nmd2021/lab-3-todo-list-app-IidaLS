f728bf72152dfd5dfe7fc700be09c1d007e00dfe
Iida <93583871+IidaLS@users.noreply.github.com>
2022-05-10 22:13:21 +0200

    testing again

--- .github/todo.js
+++ .github/todo.js
@@ -1,1 +1,18 @@
-const tasks =[""];
+const tasks = [""]
+
+
+
+
+function addTask(){
+ console.log("pressed");
+ 
+}
+const taskButtonElement = document.getElementById("taskButton");
+taskButtonElement.addEventListener("click", onClickHandler);
+
+
+const card = document.createElement("li");
+const content = document.createTextNode("inputValue")
+card.appendChild(content);
+const container = document.getElementById("container");
+container.appendChild(card);
