89a875c91ef0be4597b19f2520b57d431adf54c7
Iida <laii19xz@student.ju.se>
2022-05-11 14:30:10 +0200

    trying again

--- .github/todo.js
+++ .github/todo.js
@@ -1,7 +1,46 @@
-const tasks = [""]
+//const taskElement = getElementById="";
+const taskListElement = (getElementById = "listElement");
+
+function addTask() {
+  var li = document.createElement("li");
+  var textInputValue = document.getElementById("taskInput").value;
+  var t = document.createTextNode(textInputValue);
+  li.appendChild(t);
+  if (inputValue === "") {
+    alert("Nothing was written.");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("userInput").Value = "";
+}
+var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
 
 
 
+
+
+
+/*
+const element = document.getElementById("taskButton");
+element.addEventListener(
+  "click",
+  () => {
+    addTask();
+  } */
+
+  /*
 
 function addTask(){
  console.log("pressed");
@@ -15,4 +54,5 @@ const card = document.createElement("li");
 const content = document.createTextNode("inputValue")
 card.appendChild(content);
 const container = document.getElementById("container");
-container.appendChild(card);
+container.appendChild(card); */
+
