2665d80d5ba233b01e0981128570d7fa03f494f3
Iida <laii19xz@student.ju.se>
2022-05-20 13:30:41 +0200

    help from adina start

--- .github/todo.js
+++ .github/todo.js
@@ -1,46 +1,51 @@
+//Adina helped me with the local storage
+//variables
 let taskInputElement = document.getElementById("taskInput");
 const todoListElement = document.getElementById("todoList");
 const taskButtonElement = document.getElementById("taskButton");
+let taskId = 0;
+let tasks = [];
 
-let addTask = function () {
-  if (taskInputElement.value == "") {
-    alert("Please write a task");
-    return;
-  } else {
-    if (localStorage.tasks !== undefined) {
-      let tasksArray = JSON.parse(localStorage.tasks);
-    }
-    for (let tasks of tasksArray) {
-      var listItem = document.createElement("div");
-      listItem.className = "task";
-      listItem.innerText = taskInputElement.value;
-      todoListElement.appendChild(listItem);
-      taskInputElement.value = "";
-
-      var markButton = document.createElement("button");
-      markButton.innerText = "Done";
-      todoListElement.appendChild(markButton);
-      markButton.addEventListener("click", function () {
-        listItem.style.textDecoration = "line-through";
-      });
-
-      var deleteButton = document.createElement("button");
-      deleteButton.innerText = "Delete";
-      todoListElement.appendChild(deleteButton);
-      deleteButton.addEventListener("click", function () {
-        listItem.parentNode.removeChild(listItem);
-        deleteButton.parentNode.removeChild(deleteButton);
-        markButton.parentNode.removeChild(markButton);
-      });
-    }
+function loadPage() {
+  if (localStorage.newTask === undefined) {
+    console.log("local storage is undefined");
+    localStorage.newTask = JSON.stringify([]);
   }
-};
-taskButton.addEventListener("click", addTask);
+  let addTask = function () {
+    if (taskInputElement.value == "") {
+      alert("Please write a task");
+      return;
+    } else {
+      if (localStorage.tasks !== undefined) {
+        todoListElement = document.getElementById("taskList");
+        tasks = JSON.parse(localStorage.newTask);
+      }
 
-//local storage
-if (localStorage.tasks === undefined) {
-  localStorage.tasks = JSON.stringify([]);
+      for (let task of tasks) {
+        var listItem = document.createElement("div");
+        listItem.className = "task";
+        listItem.innerText = taskInputElement.value;
+        todoListElement.appendChild(listItem);
+        taskInputElement.value = "";
+
+        var markButton = document.createElement("button");
+        markButton.innerText = "Done";
+        todoListElement.appendChild(markButton);
+        markButton.addEventListener("click", function () {
+          listItem.style.textDecoration = "line-through";
+        });
+
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        todoListElement.appendChild(deleteButton);
+        deleteButton.addEventListener("click", function () {
+          listItem.parentNode.removeChild(listItem);
+          deleteButton.parentNode.removeChild(deleteButton);
+          markButton.parentNode.removeChild(markButton);
+        });
+      }
+    }
+  };
 }
-let tasksArray = JSON.parse(localStorage.tasks);
-tasksArray.push(tasks);
-localStorage.tasks = JSON.stringify(tasksArray);
+taskButton.addEventListener("click", addTask);
+window.addEventListener("load", loadpage);
