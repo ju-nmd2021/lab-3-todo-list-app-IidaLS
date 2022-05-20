0998dcee7b2902b8c224f748718f79ab55616c64
Iida <93583871+IidaLS@users.noreply.github.com>
2022-05-20 16:30:25 +0200

    I tried

--- .github/todo.js
+++ .github/todo.js
@@ -6,35 +6,34 @@ const taskButtonElement = document.getElementById("taskButton");
 let taskId = 0;
 let tasks = [];
 
+//loading the page
 function loadPage() {
   if (localStorage.newTask === undefined) {
     console.log("local storage is undefined");
     localStorage.newTask = JSON.stringify([]);
   }
+//alert if input is empty
   let addTask = function () {
     if (taskInputElement.value == "") {
       alert("Please write a task");
       return;
     } else {
-      if (localStorage.tasks !== undefined) {
-        todoListElement = document.getElementById("taskList");
-        tasks = JSON.parse(localStorage.newTask);
-      }
-
+      
+//add a task
       for (let task of tasks) {
         var listItem = document.createElement("div");
         listItem.className = "task";
         listItem.innerText = taskInputElement.value;
         todoListElement.appendChild(listItem);
         taskInputElement.value = "";
-
+//mark as done button
         var markButton = document.createElement("button");
         markButton.innerText = "Done";
         todoListElement.appendChild(markButton);
         markButton.addEventListener("click", function () {
           listItem.style.textDecoration = "line-through";
         });
-
+//button to delete
         var deleteButton = document.createElement("button");
         deleteButton.innerText = "Delete";
         todoListElement.appendChild(deleteButton);
@@ -49,3 +48,9 @@ function loadPage() {
 }
 taskButton.addEventListener("click", addTask);
 window.addEventListener("load", loadPage);
+
+/*
+if (localStorage.tasks !== undefined) {
+        todoListElement = document.getElementById("taskList");
+        tasks = JSON.parse(localStorage.newTask);
+      }*/
