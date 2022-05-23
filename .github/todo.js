b1d7dcd3adff6d90d1c37fc026750465a4abd2b1
Iida <93583871+IidaLS@users.noreply.github.com>
2022-05-23 18:54:17 +0200

    IT WORKS

--- .github/todo.js
+++ .github/todo.js
@@ -1,19 +1,75 @@
-//variables
-const addButton = document.getElementById("addButton");
+const addButton = document.getElementById("taskButton");
 const taskList = document.getElementsByClassName("todoList");
-const input = document.getElementById("taskInput");
+const inputField = document.getElementById("taskInput");
 let tasks; 
 
 function loadHandler(){
-    if(localStorage.tasks === undefined) {
-        localStorage.tasks =JSON.stringify([]);
-         console.log(localStorage.tasks);
-    }
-    displayList();
+  if (localStorage.tasks === undefined) {
+    localStorage.tasks = JSON.stringify([]);
+  }
+  displayList();
 }
 
+
 function displayList() {
-    console.log("view tasks");
-    tasks = JSON.parse(localStorage.tasks);
-    taskList.innerHTML = "";
+
+  tasks = JSON.parse(localStorage.tasks);
+
+  todoList.innerHTML = "";
+
+  for (let taskIndex in tasks) {
+    let task = tasks[taskIndex];
+    const taskElement = document.createElement("li");
+    taskElement.classList.add("task");
+    taskElement.innerText = task.name;
+    todoList.appendChild(taskElement);
+
+    taskElement.addEventListener("click", function () {
+      if (task.completed === false) {
+        task.completed = true;
+        updateStorage();
+        displayList();
+      }
+    });
+    if (task.completed === true) {
+      taskElement.style.textDecoration = "line-through";
+      taskElement.style.color = "var(--green)";
+
+      taskElement.addEventListener("click", function () {
+        tasks.splice(taskIndex, 1);
+        updateStorage();
+        displayList();
+      });
+    }
+  }
 }
+taskButton.addEventListener("click", function () {
+  if (inputField.value == "") {
+  alert("add task");
+  return;
+} else {
+  let newTask = {
+    name: inputField.value,
+    completed: false,
+  };
+  inputField.value = "";
+
+  if (newTask.name !== "") {
+    tasks.push(newTask);
+  }
+
+  updateStorage();
+  displayList();
+}});
+
+function updateStorage() {
+  localStorage.tasks = JSON.stringify(tasks);
+}
+inputField.addEventListener("keypress", function(event){
+  if (event.key === "Enter") {
+      event.preventDefault();
+      addButton.click();  
+  }
+});
+
+window.addEventListener("load", loadHandler);
