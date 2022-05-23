f40d28b8fe850bb81b5f8979587e31777b022ea7
Iida <93583871+IidaLS@users.noreply.github.com>
2022-05-23 20:18:05 +0200

    fixy

--- .github/todo.js
+++ .github/todo.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 //Deseada helped with both local storage and marking tasks as done
 
@@ -77,3 +78,61 @@ inputField.addEventListener("keypress", function(event){
 
 window.addEventListener("load", loadHandler);
 
+=======
+//Adina helped me with the local storage
+//variables
+let taskInputElement = document.getElementById("taskInput");
+const todoListElement = document.getElementById("todoList");
+const taskButtonElement = document.getElementById("taskButton");
+let taskId = 0;
+let tasks = [];
+
+//loading the page
+function loadPage() {
+  if (localStorage.newTask === undefined) {
+    console.log("local storage is undefined");
+    localStorage.newTask = JSON.stringify([]);
+  }
+//alert if input is empty
+  let addTask = function () {
+    if (taskInputElement.value == "") {
+      alert("Please write a task");
+      return;
+    } else {
+      
+//add a task
+      for (let task of tasks) {
+        var listItem = document.createElement("div");
+        listItem.className = "task";
+        listItem.innerText = taskInputElement.value;
+        todoListElement.appendChild(listItem);
+        taskInputElement.value = "";
+//mark as done button
+        var markButton = document.createElement("button");
+        markButton.innerText = "Done";
+        todoListElement.appendChild(markButton);
+        markButton.addEventListener("click", function () {
+          listItem.style.textDecoration = "line-through";
+        });
+//button to delete
+        var deleteButton = document.createElement("button");
+        deleteButton.innerText = "Delete";
+        todoListElement.appendChild(deleteButton);
+        deleteButton.addEventListener("click", function () {
+          listItem.parentNode.removeChild(listItem);
+          deleteButton.parentNode.removeChild(deleteButton);
+          markButton.parentNode.removeChild(markButton);
+        });
+      }
+    }
+  };
+}
+taskButton.addEventListener("click", addTask);
+window.addEventListener("load", loadPage);
+
+/*
+if (localStorage.tasks !== undefined) {
+        todoListElement = document.getElementById("taskList");
+        tasks = JSON.parse(localStorage.newTask);
+      }*/
+>>>>>>> parent of 898c732 (Merge pull request #2 from ju-nmd2021/I'm-starting-over)
